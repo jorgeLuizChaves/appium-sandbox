edcab9e53e1b9f8d0013827c7d901312b56d9034
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2014-11-19 11:15:53 -0200

    added support to IOS Devices
    added support to IOS Devices

--- src/test/java/com/br/mobinov/android/HiAppiumTest.java
+++ src/test/java/com/br/mobinov/android/HiAppiumTest.java
@@ -1,3 +1,4 @@
+package com.br.mobinov.android;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,11 +9,10 @@ import io.appium.java_client.AppiumDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.br.lab.property.MobileProperty;
+import com.br.lab.property.MobileProperty.DevicePlatform;
 
 
 public class HiAppiumTest {
@@ -21,7 +21,7 @@ public class HiAppiumTest {
 
 	@Before
 	public void setup() throws IOException{
-		DesiredCapabilities capabilities = new MobileProperty().buildCapabilities();
+		DesiredCapabilities capabilities = new MobileProperty(DevicePlatform.IOS).buildAndroidCapabilities();
 		appiumDriver = createAppiumDriver(capabilities);
 	}
 	
@@ -36,9 +36,6 @@ public class HiAppiumTest {
 	public void test() throws MalformedURLException {
 		appiumDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		
-		WebElement findElement = appiumDriver.findElement(By.name("new"));
-		
-		findElement.click();
 	}
 
 	private AppiumDriver createAppiumDriver(DesiredCapabilities capabilities)
@@ -46,16 +43,4 @@ public class HiAppiumTest {
 		AppiumDriver appiumDriver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
 		return appiumDriver;
 	}
-
-//	private DesiredCapabilities configureCapabilities() {
-//		DesiredCapabilities capabilities = new DesiredCapabilities();
-//
-//		capabilities.setCapability("device", "android");
-//		capabilities.setCapability("version", "4.x");
-//		capabilities.setCapability("deviceType", "phone");
-//		capabilities.setCapability("platformName", "Android");
-//		capabilities.setCapability("deviceName", "motorola-xt1033-T099707VK7");
-//		return capabilities;
-//	}
-
 }
